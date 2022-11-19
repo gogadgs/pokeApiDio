210f0123bb00bd3e5fe2bc034d27aef641102339
DOUGLAS GODOIS DOS SANTOS <86927046+gogadgs@users.noreply.github.com>
2022-11-19 20:21:10 -0300

    Update APIpoke.js

--- APIpoke.js
+++ APIpoke.js
@@ -1,47 +1,37 @@
 
+const pokeApi = {}
 
-const pokeApi = {};
+function convertPokeApiDetailToPokemon(pokeDetail) {
+    const pokemon = new Pokemon();
+    pokemon.number = pokeDetail.id
+    pokemon.name = pokeDetail.name
 
-function convertPokerAPIDetailToPokemon(pokeDetail){
-     var pokemon  = new Pokemon();
-    
-     pokemon.number = pokeDetail.id
-     pokemon.name = pokeDetail.name
+    const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
+   
+    const [type] = types
 
-     const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name);
+    pokemon.types = types
+    pokemon.type = pokemon.type.get[6];
+    pokemon.type=type;
 
-     const {type} = types;
+    pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
 
-
-
-    
-     pokemon.types = types
-     pokemon.type = type;
-     pokemon.photo = pokeDetail.sprites.other.dream_world.front_default;
-     return pokemon;
-
-};
-
-
-pokeApi.getPokemonDetail = (pokemon) => {
-          return fetch(pokemon.url)
-               .then((response)=>response.json())
-               .then(convertPokerAPIDetailToPokemon)
-};
-
-pokeApi.getPokemon = (offset = 0, limit = 5)=>{
-    
-    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
-    
-    
-    return fetch(url)
-    .then((response) => response.json())
-    .then((jsonBody) => jsonBody.results)
-    .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
-    .then((detailRequests) => Promise.all(detailRequests))
-    .then((pokemonsDetails) => pokemonsDetails)
+    return pokemon
 }
 
+pokeApi.getPokemonDetail = (pokemon) => {
+    return fetch(pokemon.url)
+        .then((response) => response.json())
+        .then(convertPokeApiDetailToPokemon)
+}
+
+pokeApi.getPokemons = (offset = 0, limit = 10) => {
+    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
+    return fetch(url)
+    .then((res) => res.json())
+    .then((res) => res);
 
 
-
+       
+}
